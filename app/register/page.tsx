cc7c2ac13794dab08dea57b0901a950ce7b1f553
Lado-Gloria <glorialadoo@gmail.com>
2023-10-13 14:02:02 +0300

    added the ssearchbar

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,4 @@
 
-
 'use client'
 import React, { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
--- app/systemLists/page.tsx
+++ app/systemLists/page.tsx
@@ -1,21 +1,43 @@
-
 'use client'
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../Components/Layout";
 import Link from "next/link";
 import useGetLocations from "../hooks/useGetLocations";
+import { ImSearch } from "react-icons/im"; 
 
 const SystemlistsPage = () => {
   const { locations } = useGetLocations();
+  const [searchQuery, setSearchQuery] = useState(""); 
+
+  const filteredLocations = locations.filter((location) =>
+    location.region_name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div>
       <Layout>
-        <h1 className="text-4xl text-green font-bold mb-4 text-center mt-10">
-          SYSTEM LISTS
-        </h1>
-        <div className="w-full flex justify-center mt-10">
-          <table className="w- table-auto rounded-lg overflow-hidden shadow-lg">
+        <div className="flex justify-between items-center mb-20">
+          <h1 className="text-4xl text-green font-bold  mt-10 " style={{marginLeft:300}}>
+            SYSTEM LISTS
+          </h1>
+          {/* Search Bar */}
+          <div className="relative mr-40 mt-10" style={{marginRight:300}}>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="py-2 pl-10 pr-20 border rounded-lg border-black font-light focus:outline-none"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <ImSearch className="text-darkGreen text-2xl" />
+            </div>
+          </div>
+          {/* End Search Bar */}
+        </div>
+        <div className="w-full flex justify-center mt-6">
+          <table className="w-table-auto rounded-lg overflow-hidden shadow-lg">
+            {/* Table Header */}
             <thead>
               <tr className="bg-gray">
                 <th className="px-6 py-3 pl-50 text-left text-lg text-green tracking-wider">
@@ -29,10 +51,10 @@ const SystemlistsPage = () => {
                 </th>
               </tr>
             </thead>
+            {/* Table Body */}
             <tbody>
-
-              {Array.isArray(locations) && locations.length > 0 ? (
-                locations.map((location) => (
+              {Array.isArray(filteredLocations) && filteredLocations.length > 0 ? (
+                filteredLocations.map((location) => (
                   <tr
                     key={location.id}
                     className="bg-gray-100 hover:bg-gray transition duration-300"
@@ -44,7 +66,7 @@ const SystemlistsPage = () => {
                       {location.region_name}
                     </td>
                     <td className="px-6 py-4 border-b border-gray text-left pr-6 pl-80 text-green">
-                    {location. installation_date}
+                      {location.installation_date}
                     </td>
                   </tr>
                 ))
@@ -64,107 +86,3 @@ const SystemlistsPage = () => {
 };
 
 export default SystemlistsPage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import Layout from '../Components/Layout';
-// import Link from 'next/link';
-
-// const SystemlistsPage = () => {
-//   const systemList = [
-//     { id: '001', location: 'Kisumu', status: 'Working' },
-//     { id: '002', location: 'Kariok', status: 'Not working' },
-//     { id: '003', location: 'Nakuru', status: 'Working' },
-//     { id: '004', location: 'Karen', status: 'Working' },
-//     { id: '005', location: 'Kikuyu', status: 'Not working' },
-//     { id: '006', location: 'BabaDogo', status: 'Working' },
-//     { id: '007', location: 'Kawangware', status: 'Working' },
-//     { id: '008', location: 'AkiraChix', status: 'Not working' },
-//     { id: '009', location: 'Kakamega', status: 'Working' },
-//     { id: '010', location: 'Mombasa', status: 'Working' },
-//   ];
-//   return (
-//     <div>
-//       <Layout>
-//         <h1 className="text-4xl text-green font-bold mb-4 text-center mt-10">
-//           SYSTEM LISTS
-//         </h1>
-//         <div className="w-full flex justify-center mt-10">
-//           <table className="w- table-auto rounded-lg overflow-hidden shadow-lg">
-//             <thead>
-//               <tr className="bg-gray">
-//                 <th className="px-6 py-3 pl-50 text-left text-lg text-green tracking-wider">
-//                   System ID
-//                 </th>
-//                 <th className="px-6 py-3 pl-60 text-left text-lg text-green tracking-wider">
-//                   Location
-//                 </th>
-//                 <th className="px-6 pl-80 py-3 text-left text-lg text-green tracking-wider">
-//                   Status
-//                 </th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {systemList.map((system) => (
-//                 <tr
-//                   key={system.id}
-//                   className="bg-gray-100 hover:bg-gray transition duration-300"
-//                 >
-//                   <td className="px-6 py-4 border-b border-gray text-left pr-6 pl-50">
-//                     <Link href={`systemData`}>{system.id}</Link>
-//                   </td>
-//                   <td className="px-6 py-4 border-b border-gray text-left pr-6 pl-60">
-//                     {system.location}
-//                   </td>
-//                   <td
-//                     className={`px-6 py-4 border-b border-gray text-left pr-6 pl-80 ${
-//                       system.status === 'Working' ? 'text-green' : 'text-red'
-//                     }`}
-//                   >
-//                     {system.status}
-//                   </td>
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         </div>
-//       </Layout>
-//     </div>
-//   );
-// };
-
-// export default SystemlistsPage;
-
-
-
-// SystemlistsPage.js
